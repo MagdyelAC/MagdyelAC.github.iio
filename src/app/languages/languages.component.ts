3c550918ffc88ba3d14b4d15374fcec036efb59e
MagdyelAC <MagdyelAguilarCid.com>
2022-09-12 14:53:48 -0500

    languages #15

--- src/app/languages/languages.component.ts
+++ src/app/languages/languages.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./languages.component.css']
 })
 export class LanguagesComponent implements OnInit {
+  languages : Array<any> = []
 
   constructor() { }
 
   ngOnInit(): void {
+    let l = {
+      nombreL: "Español",
+      };
+      let l2 = {
+        nombreL: "Ingles",
+        };
+      this.languages.push(1);
+      this.languages.push(l2);
+
   }
+  
 
 }
