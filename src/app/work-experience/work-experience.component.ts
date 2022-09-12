36619b5d98e92ddb611077010eedfa5416cd62e5
MagdyelAC <MagdyelAguilarCid.com>
2022-09-12 11:22:55 -0500

    work_experience #3

--- src/app/work-experience/work-experience.component.ts
+++ src/app/work-experience/work-experience.component.ts
@@ -6,10 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./work-experience.component.css']
 })
 export class WorkExperienceComponent implements OnInit {
-
+  workExperiencies : Array<any> = [];
   constructor() { }
 
   ngOnInit(): void {
+    let work1 = {
+      fecha: "2021-2022",
+      ubicacion: "Tlilapan.Ver",
+      puesto: "Ingeniero en sistemas",
+      empresa: "DIF",
+      logros:[
+        {descripcion: "Creacion de documentos digitales, asesoramiento a empleados,construccion de metodos y componentes importantes para la empresa"},
+        {descripcion: "Creacion de metodos faciles para los empleados."}
+      ]
+    };
+
+    let work2 = {
+      fecha: "2020-2021",
+      ubicacion: "Tlilapan.Ver",
+      puesto: "Complemento de sistemas",
+      empresa: "Aple",
+      logros:[
+        {descripcion: "Coordinar a los equipos de servicios profesionales y ciencia de datos de Aple"},
+        {descripcion: "Analizar datos y estadisticas sobre los clientes"}
+      ]
+    };
   }
 
 }
